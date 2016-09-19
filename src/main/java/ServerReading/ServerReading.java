0fc826a308d9930fb7f6d2551aefcfb9cde229e9
angelless <dongkyu91622@gmail.com>
2016-09-19 12:37:01 +0900

    Solo5Star  issues
    Commit

--- src/main/java/ServerReading/ServerReading.java
+++ src/main/java/ServerReading/ServerReading.java
@@ -11,7 +11,7 @@ public class ServerReading extends PluginBase implements Listener {
 	public void onEnable() {
 		this.getLogger().info("ServerReading플러그인 활성화!");
 		this.getServer().getPluginManager().registerEvents(this, this);
-		this.getServer().getScheduler().scheduleRepeatingTask(new ReadingTask(this), 15);
+		this.getServer().getScheduler().scheduleRepeatingTask(new ReadingTask(this), 40);
 	}
 
 }
